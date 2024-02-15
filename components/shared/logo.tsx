497a7be3e53eef363fc846b4a0af1ff2d2d890b6
Dimas hilmawan <hilmawan22@gmail.com>
2024-02-15 17:51:26 +0700

    final adjustment

--- components/shared/logo.tsx
+++ components/shared/logo.tsx
@@ -4,15 +4,15 @@ import Link from "next/link";
 export const Logo = () => {
   return (
     <Link href="/">
-      <div className="flex-center gap-1">
+      <div className="flex-center gap-2">
         <Image
           src="/assets/images/eventbyte-logo.png"
           alt="Logo Image"
           width={128}
           height={38}
-          className="-ml-2 h-auto w-[4.5rem]"
+          className="h-auto w-[2.75rem]"
         />
-        <p className="-ml-3 font-poppins text-lg font-semibold text-[#003c80]">
+        <p className=" font-poppins text-lg font-semibold text-[#003c80]">
           EventByte
         </p>
       </div>
